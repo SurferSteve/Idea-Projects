43d6287e1d13eb9df564e031e04281a4330a171a
SurferSteve <borysovs@gmail.com>
2016-05-18 19:02:21 +0300

    Java1Lesson3DataTypes

--- javadive0816/l1_test/src/Java1Lesson3DataTypes.java
+++ javadive0816/l1_test/src/Java1Lesson3DataTypes.java
@@ -2,4 +2,12 @@
  * Created by Borisov on 12.04.2016.
  */
 public class Java1Lesson3DataTypes {
+    public static void main(String[] args) {
+        byte tiny = Byte.MIN_VALUE;
+//        System.out.println(tiny);
+        tiny = (byte) (tiny - 3);
+        System.out.println(tiny);
+        float float1 = Float.MIN_NORMAL;
+        System.out.println(float1);
+    }
 }
